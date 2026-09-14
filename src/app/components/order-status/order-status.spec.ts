b396ec73cbd70f32a8ba1e37b8b00fc06f54fda0
Leonardo Ferreira <leonardo.gini.96@hotmail.com>
2026-09-13 22:34:06 -0300

    refactor: ajuste consulta pedidos

--- src/app/components/order-status/order-status.spec.ts
+++ src/app/components/order-status/order-status.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 import { OrderStatus } from './order-status';
 
@@ -9,6 +11,7 @@ describe('OrderStatus', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [OrderStatus],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(OrderStatus);
